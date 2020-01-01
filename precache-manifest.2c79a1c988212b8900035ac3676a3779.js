593f6c1101f899e9dcb2b80b6e6c58681eaf7c0d
Karol-Wroblewski <Karol-Wroblewski@users.noreply.github.com>
2020-01-01 13:33:39 +0100

    Updates

---
+++ precache-manifest.2c79a1c988212b8900035ac3676a3779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e92988b8ee0aa847f44d176211e9b0",
+    "url": "/recruitment-task/index.html"
+  },
+  {
+    "revision": "c11beabf7023460b81ac",
+    "url": "/recruitment-task/static/css/2.b592541c.chunk.css"
+  },
+  {
+    "revision": "b0caa135a357b46725d4",
+    "url": "/recruitment-task/static/css/main.07dcbbff.chunk.css"
+  },
+  {
+    "revision": "c11beabf7023460b81ac",
+    "url": "/recruitment-task/static/js/2.b41ba6bc.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/recruitment-task/static/js/2.b41ba6bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0caa135a357b46725d4",
+    "url": "/recruitment-task/static/js/main.a07598d0.chunk.js"
+  },
+  {
+    "revision": "5f5c33c76558d5550d99",
+    "url": "/recruitment-task/static/js/runtime-main.3526a695.js"
+  }
+]);
